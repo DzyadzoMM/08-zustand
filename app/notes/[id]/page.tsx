06e2918b1f6caa74c883c126124a8a0fd6c51c21
DzyadzoMM <m.dziadzo@gmail.com>
2025-09-05 19:18:05 +0300

    zustand

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
 import NoteDetailsClient from "@/app/notes/[id]/NoteDetails.client";
+import type { Metadata } from "next";
 
 type Props = {
   params: Promise<{ id: string }>;
